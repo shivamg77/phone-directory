89614dcc15644d64571dd35bd1e8e64b6f8d79d4
shivam <shivamgoyal.005@gmail.com>
2019-12-06 23:01:18 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
 
-class AddSusbscriber extends Component {
+class AddSubscriber extends Component {
     
     constructor() {
         super();
@@ -19,26 +19,32 @@ class AddSusbscriber extends Component {
         this.setState(state);
     }
 
+    onFormSubmitted = (e) => {
+        e.preventDefault();
+        this.props.addSubscriberHandler(this.state);
+        this.setState({ id: 0, name: '', phone: ' ' });
+    }
+
     render() {
         
-        const {name, phone} = this.state;
+        const { name, phone } = this.state;
 
         return (
             <div>
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
                     <button className="custom-btn">Back</button>
-
-                    <form className="subscriber-form">
-                        <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler}/><br/><br/>
-                        <input id="name" type="text" className="input-control" name="name"/><br/><br/>
-                        <label htmlFor="phone" className="label-control">Phone: </label><br/>
-                        <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangedHandler}/><br/><br/>
+  
+                    <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
+                        <label htmlFor="name" className="label-control">Name: </label><br />
+                        <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler} /><br /><br />
+                        <label htmlFor="phone" className="label-control">Phone: </label><br />
+                        <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangedHandler} /><br /><br />
 
                         <div className="subscriber-info-container">
-                            <span className="subscriber-to-add-heading">Subscriber to be added: </span><br/>
-                            <span className="subscriber-info">Name: {name}</span><br/>
-                            <span className="subscriber-info">Phone: {phone}</span><br/>
+                            <span className="subscriber-to-add-heading">Subscriber to be added: </span><br />
+                            <span className="subscriber-info">Name: {name}</span><br />
+                            <span className="subscriber-info">Phone: {phone}</span><br />
                         </div>
 
                         <button type="submit" className="custom-btn add-btn">Add</button>
@@ -49,4 +55,4 @@ class AddSusbscriber extends Component {
     }
 }
 
-export default AddSusbscriber; 
+export default AddSubscriber; 

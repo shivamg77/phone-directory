a29380b3d517db9a6673db5526babd739683c5b4
shivam <shivamgoyal.005@gmail.com>
2019-12-06 23:07:10 +0530

    Routing: Added routing for Add Subscriber page and Show Subscribers page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AddSubscriber from './AddSubscriber';
 import './common/common.css';
 import PhoneDirectory from './PhoneDirectory';
 
